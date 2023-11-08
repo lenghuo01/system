4d33f5221946798727bf84d6f5a8c5bfa231715a
lenghuo01 <1946949433@qq.com>
2023-11-08 23:40:48 +0800

    j

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,13 +4,13 @@ import { useRouter ,useRoute} from 'vue-router';
 export default{
     name:'MainComp',
     setup(){
-        //传输参数
-        const router = useRouter()
-        //接收参数
-        const route=useRoute()
-        onMounted(()=>{
-            console.log(route.query)
-        })
+        // //传输参数
+         const router = useRouter()
+        // //接收参数
+        // const route=useRoute()
+        // onMounted(()=>{
+        //     console.log(route.query)
+        // })
         function bu(){
             router.push('/login')
         }
@@ -22,31 +22,55 @@ export default{
 }
 </script>
 <template>
-    <a @click="bu">点我</a>
+ 
+    <!-- <div class="video-background">
+    <video autoplay muted loop id="bgVideo" >
+      <source src="./img/beijing.mp4" type="video/mp4">
+    </video>
+ 
+ 
+     <a @click="bu">点击登录</a>
+  </div> -->
+  <div class="container">
+    <video
+      class="video-bg"
+      loop
+      autoplay
+      muted
+      preload="auto"
+      src="./img/beijing.mp4"
+    ></video>
+    <div class="main-body">
+      <div class="title">激发创造，丰富生活</div>
+      <div ><a @click="bu">点击登录</a></div>
+    </div>
+  </div>
  
 </template>
 <style scoped>
+
     * {
     margin:0;
     padding:0;
     }
      
     a {
- 
+      align-items: center;
     text-decoration: none;
     position: absolute;
     left:50%;
     top:50%;
     transform:translate(-50%,-50%);
-    font-size: 24px;
+    font-size: 18px;
     background:linear-gradient(90deg,#009DF7,#FF58A5,#FFE431,#009EFC);
-    width:400px;
-    height:100px;
+    width:156px;
+    height:54px;
     color:white;
     text-align:center;
-    line-height:100px;
+    line-height:50px;
     border-radius:50px;
     background-size: 400%;
+    animation:sun 8s infinite;
     }
     
     a::before{
@@ -61,6 +85,7 @@ export default{
         filter:blur(20px);
         z-index:-1;
         background-size:400%;
+        animation:sun 8s infinite;
     }
     a:hover{
         animation:sun 8s infinite;
@@ -73,4 +98,53 @@ export default{
             background-position:-400% 0;
         }
     }
+    .container {
+  height: 100%;
+  overflow: hidden;
+}
+
+.video-bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  object-fit: cover;
+  z-index: -1;
+}
+
+.main-body {
+  margin-top: 250px;
+  width: 100%;
+  display: flex;
+
+  flex-wrap: wrap;
+  justify-content: center;
+
+  .title {
+    text-align: center;
+    width: 100%;
+    font-size: 56px;
+    color: white;
+    margin-bottom: 40px;
+    font-weight: 600;
+  }
+}
+
+.login-btn {
+  width: 156px;
+  height: 54px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 27px;
+  background-color: white;
+  color: #00b86b;
+  font-size: 14px;
+
+  &:hover {
+    cursor: pointer;
+
+    color: #11975f;
+  }
+}
 </style>
