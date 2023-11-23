94f16dcac1e4bd93e2a3d16ac331bea51f359c99
lenghuo01 <1946949433@qq.com>
2023-11-23 17:45:57 +0800

    hh

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import echarts from 'rollup-plugin-echarts'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,11 +10,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    rollupOptions: {
-      plugins: [echarts()]
     }
   },
 })
